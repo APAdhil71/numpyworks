dc1f010955200c4f5ea6689abc7f0e7718e149dd
Adhil2004 <Adhiladhi4110@gmail.com>
2026-01-08 14:59:59 +0530

    added

---
+++ array_creation.py
@@ -0,0 +1,13 @@
+import numpy as np
+# fours_array=np.full((5,5),4)
+# print(fours_array)
+# array=np.ones((3,3),dtype="int16")
+# print(array)
+# array[1,1]=100
+# print(array)
+# fours_array[1:4,1:4]=array
+# print(fours_array)
+rand_array=np.random.rand(2,4)
+print(rand_array)
+rand_int_array=np.random.randint(10,15,(3,3))
+print(rand_int_array)
